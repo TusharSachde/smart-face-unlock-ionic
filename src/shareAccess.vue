1f970a27836c04824adc536cda8f1e828013d7a5
Tushar Sachde <tushar.sachde@sptr.co>
2018-06-06 11:37:55 +0530

    Finishing up

--- src/shareAccess.vue
+++ src/shareAccess.vue
@@ -1,21 +1,68 @@
 <template>
   <v-ons-page>
     <custom-toolbar>Smart Doors</custom-toolbar>
-    <p style="text-align: center">
-      <img src="imgSrc" v-if="imgSrc">
-      <v-ons-button modifier="large" style="margin: 6px 0">Unlock Door</v-ons-button>
-      <v-ons-button modifier="large" style="margin: 6px 0">Share Access</v-ons-button>
-    </p>
+          <v-ons-list>
+
+<v-ons-list-header>Setup Server</v-ons-list-header>
+    <v-ons-list-item>
+      <div class="center">
+        <v-ons-input placeholder="Please enter server IP" float
+          v-model="serverIP"
+        >
+        </v-ons-input>
+      </div>
+    </v-ons-list-item>
+    <v-on-list-item>
+      <div style="padding: 10px;">
+      <v-ons-button modifier="large cta" @click="saveServerIP">Update</v-ons-button>
+      </div>
+    </v-on-list-item>
+
+        <v-ons-list-header>Setup Bridge</v-ons-list-header>
+    <v-ons-list-item>
+      <div class="center">
+        <v-ons-input placeholder="Please enter bridge IP" float
+          v-model="bridgeIP"
+        >
+        </v-ons-input>
+      </div>
+    </v-ons-list-item>
+    <v-on-list-item>
+      <div style="padding: 10px;">
+      <v-ons-button modifier="large cta" @click="sendBridgeIP">Send Bridge IP</v-ons-button>
+      </div>
+    </v-on-list-item>
+     
+  </v-ons-list>
+<div style="text-align: center;padding:10px;">
+      <v-ons-button modifier="large cta" style="margin: 6px 0;" @click="unlockDoor">Unlock Door</v-ons-button>
+      <v-ons-button modifier="large cta" style="margin: 6px 0;" @click="openCamera">Share Access</v-ons-button>
+      <v-ons-button modifier="large cta" style="margin: 6px 0;" @click="getBluetoothInfo">Get Bluetooth Info</v-ons-button>
+      {{adapterInfo}}
+      <img src="imgSrc" v-if="imgSrc" style="width:100%; max-height:300px;">
+      {{imgSrc}}
+    </div>
   </v-ons-page>
 </template>
 
 <script>
 import customToolbar from "./CustomToolbar";
+import axios from "axios";
+
 export default {
   data() {
     return {
-      imgSrc: ""
+      imgSrc: "demo",
+      bridgeIP: "",
+      serverIP: "",
+      adapterInfo: {}
     };
+  },
+  mounted() {
+    var localBridgeIP = localStorage.getItem("bridgeIP");
+    var localServerIP = localStorage.getItem("serverIP");
+    this.bridgeIP = localBridgeIP ? localBridgeIP : "0.0.0.0";
+    this.serverIP = localServerIP ? localServerIP : "0.0.0.0";
   },
   methods: {
     openCamera: function() {
@@ -25,11 +72,11 @@ export default {
         destinationType: Camera.DestinationType.DATA_URL,
         sourceType: Camera.PictureSourceType.CAMERA,
         encodingType: Camera.EncodingType.JPEG,
-        targetWidth: 900,
+        targetItemWidth: 900,
         correctOrientation: true
       };
 
-      navigator.camera.getPicture(
+      navigator.camera.getItemPicture(
         function(imageData) {
           console.log(imageData);
           this.imgSrc = imageData;
@@ -40,9 +87,49 @@ export default {
         cameraOptions
       );
     },
-    sendImage: function() {
+    getBluetoothInfo: function() {
+      networking.bluetooth.getAdapterState(
+        function(adapterInfo) {
+          // The adapterInfo object has the following properties:
+          // address: String --> The address of the adapter, in the format 'XX:XX:XX:XX:XX:XX'.
+          // name: String --> The human-readable name of the adapter.
+          // enabled: Boolean --> Indicates whether or not the adapter is enabled.
+          // discovering: Boolean --> Indicates whether or not the adapter is currently discovering.
+          // discoverable: Boolean --> Indicates whether or not the adapter is currently discoverable.
+          console.log(
+            "Adapter " + adapterInfo.address + ": " + adapterInfo.name
+          );
+          this.adapterInfo = adapterInfo;
+        },
+        function(errorMessage) {
+          console.error(errorMessage);
+        }
+      );
+    },
+    sendBridgeIP: function() {
+      localStorage.setItem("bridgeIP", this.bridgeIP);
+      var url = `http://${this.serverIP}/Nukilock/setItemNukilockBridgeUrl`;
+      console.log(url);
       axios
-        .get(`http://jsonplaceholder.typicode.com/posts`)
+        .post(url, {
+          params: this.bridgeIP
+        })
+        .then(response => {
+          // JSON responses are automatically parsed.
+          console.log(response);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    saveServerIP: function() {
+      localStorage.setItem("serverIP", this.serverIP);
+      console.log(this.serverIP);
+    },
+    unlockDoor: function() {
+      var url = `http://${this.serverIP}/Nukilock/unlockNukilock`;
+      axios
+        .post(url)
         .then(response => {
           // JSON responses are automatically parsed.
           console.log(response);
